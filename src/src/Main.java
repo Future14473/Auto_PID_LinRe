900b5f2b486bc2132a966921843481ba8276cdcd
kyleng <ng.kyle101@gmail.com>
2021-09-04 02:18:12 -0700

    StandardDev works

---
+++ src/src/Main.java
@@ -0,0 +1,8 @@
+import Utils.StandardDev;
+
+public class Main {
+    public static void main (String[] args){
+//        double[] ourLoss = {1,2,2,3,4};
+//        System.out.println(StandardDev.getStandardDev(ourLoss));
+    }
+}
---
+++ src/src/Utils/StandardDev.java
@@ -0,0 +1,23 @@
+package Utils;
+
+public class StandardDev {
+    public static double getStandardDev(double[] loss){
+        double avg = getAverageArray(loss);
+
+        double sum = 0;
+
+        for (double val : loss){
+            sum += Math.pow( val - avg , 2.0);
+        }
+        return Math.sqrt(sum/(loss.length-1));
+    }
+
+    static double getAverageArray(double[] arr){
+        double sum = 0;
+        for (double val : arr){
+            sum += val;
+        }
+
+        return sum/arr.length;
+    }
+}
