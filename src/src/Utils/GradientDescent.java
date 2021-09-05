e77c51f954832ab5708dea02090dd6ce29ccbe28
kyleng <ng.kyle101@gmail.com>
2021-09-04 20:52:29 -0700

    Use dCost instead of raw cost val

--- src/src/Utils/GradientDescent.java
+++ src/src/Utils/GradientDescent.java
@@ -1,8 +1,8 @@
 package Utils;
 
 public class GradientDescent {
-    double learningRate;
-    double tunerVar;
+    double learningRate, currentTunerVar;
+    double lastCostVal, currentCostVal, lastTunerVar;
     /*
      * Gradient descent is the process of optimizing the next guess for kP
      * Iterative optimizing algorithm for finding a local minimum of a differential function
@@ -15,25 +15,47 @@ public class GradientDescent {
 
     public GradientDescent(double startLearningRate , double startTunerVar){
         learningRate = startLearningRate;
-        tunerVar = startTunerVar;
+        currentTunerVar = startTunerVar;
+        // run
+        // calc cost
+        // set last tuner var to current tuner var; set current tuner var to arbitrary number
     }
 
     /**
      * Uses Gradient descent to get next tuner variable
      */
 
+    /*possibly add Gradient Descent with momentum in the future*/
     public void getNextTunerVar(double[] loss, double learningRate){
         //data and cost done in AS later
-        double costVal = getCost(loss);
-        tunerVar += costVal * learningRate;
+        currentCostVal = getCost(loss);
+        double dCost = (currentCostVal - lastCostVal) / (currentTunerVar - lastTunerVar); // derivative of cost fn at current point in time
+        currentTunerVar -= dCost * learningRate; //negative because opposite direction of derivative (going downhill(-) increase tunerVar(+))
+
+        // save previous run values
+        lastTunerVar = currentTunerVar;
+        lastCostVal = currentCostVal;
     }
 
     public double getCost(double[] loss){
         return StandardDev.getStandardDev(loss);
     }
 
-    public double getTunerVar() {
-        return tunerVar;
+    public void setLastCostVal(double lastCostVal) {
+        this.lastCostVal = lastCostVal;
+    }
+
+    public void setCurrentTunerVar(double currentTunerVar) {
+        this.currentTunerVar = currentTunerVar;
+    }
+
+    public void setLastTunerVar(double lastTunerVar) {
+        this.lastTunerVar = lastTunerVar;
+    }
+
+
+    public double getCurrentTunerVar() {
+        return currentTunerVar;
     }
 
     public void setLearningRate(double learningRate) {
