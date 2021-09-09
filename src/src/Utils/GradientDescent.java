aac5ff54ed2881cd973d67828cf63407b17aa6de
kyleng <ng.kyle101@gmail.com>
2021-09-09 10:45:32 -0700

    From AS tuning

--- src/src/Utils/GradientDescent.java
+++ src/src/Utils/GradientDescent.java
@@ -1,8 +1,15 @@
-package Utils;
+package org.firstinspires.ftc.teamcode.drive.opmode.gradde.mathUtils;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+import java.util.ArrayList;
 
 public class GradientDescent {
-    double learningRate, currentTunerVar;
-    double lastCostVal, currentCostVal, lastTunerVar;
+    double learningRate, currentTunerVar, lastCostVal, currentCostVal, lastTunerVar;
+    Telemetry telemetry;
     /*
      * Gradient descent is the process of optimizing the next guess for kP
      * Iterative optimizing algorithm for finding a local minimum of a differential function
@@ -13,7 +20,8 @@ public class GradientDescent {
     //          guess and error(in coding train video) is actually our Cost function because it tells us how off we are
     // 3) Update tuning constant kTunerVar += Cost(kTunerVar Current) * learning rate
 
-    public GradientDescent(double startLearningRate , double startTunerVar){
+    public GradientDescent(double startLearningRate , double startTunerVar, Telemetry telemetry){
+        this.telemetry = telemetry;
         learningRate = startLearningRate;
         currentTunerVar = startTunerVar;
         // run
@@ -26,20 +34,39 @@ public class GradientDescent {
      */
 
     /*possibly add Gradient Descent with momentum in the future*/
-    public void getNextTunerVar(double[] loss, double learningRate){
-        //data and cost done in AS later
-        currentCostVal = getCost(loss);
+    public double getNextTunerVar(double currentCostVal, double lastCostVal, double currentTunerVar, double lastTunerVar){
+//    public void getNextTunerVar(ArrayList<Double> loss){
+        telemetry.addData("Current Cost Val", currentCostVal);
+        telemetry.addData("Last Cost Val", lastCostVal);
+        telemetry.addData("Current Tuner Val", currentTunerVar);
+        telemetry.addData("Last Tuner Val", lastTunerVar);
+
         double dCost = (currentCostVal - lastCostVal) / (currentTunerVar - lastTunerVar); // derivative of cost fn at current point in time
         currentTunerVar -= dCost * learningRate; //negative because opposite direction of derivative (going downhill(-) increase tunerVar(+))
 
-        // save previous run values
-        lastTunerVar = currentTunerVar;
-        lastCostVal = currentCostVal;
+        telemetry.addData("D Cost ", dCost);
+        telemetry.addData("Returned currentTunerVar ", currentTunerVar);
+        telemetry.addData("PID Translation", SampleMecanumDrive.TRANSLATIONAL_PID.toString());
+        telemetry.update();
+        return currentTunerVar;
     }
 
-    public double getCost(double[] loss){
+//    private ArrayList<Double> converToDoubleArray(ArrayList<Pose2d> list){
+//        ArrayList<Double> dblList = new ArrayList<>();
+//        for (Pose2d pos:
+//             list) {
+//            dblList.add(pos.getHe)
+//        }
+//    }
+
+    public double getCost(ArrayList<Double> loss){
         return StandardDev.getStandardDev(loss);
     }
+
+    public void setCurrentCostVal(double currentCostVal) {
+        this.currentCostVal = currentCostVal;
+    }
+
 
     public void setLastCostVal(double lastCostVal) {
         this.lastCostVal = lastCostVal;
