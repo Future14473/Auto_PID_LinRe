1f74d2fc5ed750db20b77db2a7a2b90e91e9208a
kyleng <ng.kyle101@gmail.com>
2021-09-09 15:48:14 -0700

    Seems to be erroring for some certain cases, need to graph for visualization

--- src/src/Main.java
+++ src/src/Main.java
@@ -2,6 +2,7 @@ import Utils.GradientDescent;
 import Utils.StandardDev;
 
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args){
@@ -9,20 +10,20 @@ public class Main {
 //        System.out.println(StandardDev.getStandardDev(ourLoss));
 
         // rand initialized values
-        double startLearningRate = 0.02;
-
-        GradientDescent testGrad = new GradientDescent(startLearningRate);
+        double learningRate = 0.02;
+        GradientDescent testGrad = new GradientDescent(learningRate);
 
         // first 2 kV and Cost then you can loop
-        double lastTunerVar = 5, lastLoss = testFunc(lastTunerVar);
+        double lastTunerVar = -5, lastLoss = testFunc(lastTunerVar);
 
         //current tuner var is just our manually inputted next guess
-        double currentTunerVar = 4, currentLoss = testFunc(currentTunerVar);
+        double currentTunerVar = -4, currentLoss = testFunc(currentTunerVar);
 
-        for (int i = 0; i < 500; i++) {
+        int epochs = 30;
+        for (int i = 0; i < epochs; i++) {
             double nextTunerVar = testGrad.getNextTunerVar(currentLoss, lastLoss,currentTunerVar, lastTunerVar);
 
-            System.out.println("Current Iteration: " + i + 1);
+            System.out.println("Current Iteration: " + (i + 1));
             System.out.println("Current Loss: " + currentLoss);
             System.out.println("Current Tuner Var: " + nextTunerVar);
 
@@ -34,6 +35,7 @@ public class Main {
     }
     // quadratic fn
     static double testFunc(double n){
-        return Math.pow(n, 3);
+//        return Math.pow(n, 10);
+        return Math.pow(n, 3) + Math.pow(n, 2) + 7;
     }
 }
