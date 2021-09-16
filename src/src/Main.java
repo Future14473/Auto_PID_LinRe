ae0ff6ce17954e12c909fd4520cbbf3c4996124a
kyleng <ng.kyle101@gmail.com>
2021-09-16 12:19:22 -0700

    Modularized Gradient Descent Tuner

--- src/src/Main.java
+++ src/src/Main.java
@@ -14,33 +14,26 @@ public class Main {
         ActualGraph actualGraph = new ActualGraph();
         Scanner scanner = new Scanner(System.in);
 
-
-
-        // Loss Capturer is a function for modeling purposes
+        // Loss Recorder is a function for modeling purposes in Java only code
         ModelFunction modelFunction = new ModelFunction(FunctionType.QUADRATIC, 1, FunctionType.NO_FLIP);
-
-
-        // first 2 kV and Cost then you can loop
-        double lastTunerVar = 5, lastLoss = modelFunction.getLoss(lastTunerVar);
-
-        //current tuner var is just our manually inputted next guess
-        double currentTunerVar = 4, currentLoss = modelFunction.getLoss(currentTunerVar);
 
         GradientDescentTuner kV_Tuner = new GradientDescentTuner(0.02, modelFunction, 5, 4); //TODO have the gradient descent run get loss from these values tumer 1, 2
         for (int i = -10; i < 11; i++) {
             actualGraph.addDataToChart(i, modelFunction.getFunctionVal(i));
         }
 
-        int i = 0;
-        while(! scanner.nextLine().equals(" ") && (i < 32)){
-            double nextTunerVar = kV_Tuner.getNextTunerVar(currentLoss, lastLoss,currentTunerVar, lastTunerVar);
-
+        int i = 2; // already 2 test done when initializing GradientDescentTuner
+        while(! scanner.nextLine().equals(" ") && (i < 34)){
+            kV_Tuner.printVals();
             System.out.println("Current Iteration: " + (i++));
-            System.out.println("Current Loss: " + currentLoss);
-            System.out.println("Current Tuner Var: " + nextTunerVar);
 
-            myChart.addDataToChart(currentTunerVar, currentLoss);
+            kV_Tuner.calculateNextTunerVar();
+            kV_Tuner.update();
+
+            myChart.addDataToChart(kV_Tuner.getCurrentTunerVar(), kV_Tuner.getCurrentCost());
         }
-        System.out.println("Current Iteration " + i + " | Final TunerVar " + currentTunerVar + " | Final Loss: " + currentLoss);
+
+        scanner.close();
+        System.out.println("Current Iteration " + i + " | Final TunerVar " + kV_Tuner.getCurrentTunerVar() + " | Final Loss: " + kV_Tuner.getCurrentCost());
     }
 }
--- src/src/Utils/GradientDescentTuner.java
+++ src/src/Utils/GradientDescentTuner.java
@@ -9,9 +9,13 @@ public class GradientDescentTuner {
     ModelFunction costRecorder; // TODO make this an interface
 
     // note costCapturer will be replaced with robot hardware later
-    public GradientDescentTuner(double learningRate, ModelFunction costCapturer, double tuner1, double Cost1, double tuner2, double Cost2){
-//TODO constructor should probably call update after calculating first vals of geNextCost,
-// and then the calculate Next Tuner Var?
+    public GradientDescentTuner(double learningRate, ModelFunction costRecorder, double tuner1, double tuner2){
+        this.learningRate = learningRate;
+        this.costRecorder = costRecorder;
+        this.lastTunerVar = tuner1;
+        this.currentTunerVar = tuner2;
+        this.lastCost = costRecorder.getLoss(lastTunerVar);
+        this.currentCost = costRecorder.getLoss(currentTunerVar);
     }
 
     public void calculateNextTunerVar(){
@@ -25,10 +29,23 @@ public class GradientDescentTuner {
         currentCost = costRecorder.getLoss(nextTunerVar); //TODO Get next loss
     }
 
+    public void printVals(){
+
+        System.out.println("Current Loss: " + currentCost);
+        System.out.println("Current Tuner Var: " + nextTunerVar);
+    }
+
     double getDerivCost(){
         return getDerivative(lastTunerVar, lastCost, currentTunerVar, currentCost);
     }
     double getDerivative(double x1, double y1, double x2, double y2){
         return (y1-y2)/ (x1-x2);
+    }
+
+    public double getCurrentTunerVar() {
+        return currentTunerVar;
+    }
+    public double getCurrentCost() {
+        return currentCost;
     }
 }
